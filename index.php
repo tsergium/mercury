8b4b8679979541b87facad86b776bf1ce4edad15
tsergium <tsergium@gmail.com>
2017-10-13 13:35:33 +0200

    test change

--- index.php
+++ index.php
@@ -3,6 +3,9 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App();
 
+$app->get('/', function ($request, $response, $args) {
+   echo 'Hello homepage';
+});
 $app->get('/{name}', function ($request, $response, $args) {
     echo "Hello, " . $args['name'];
 });
